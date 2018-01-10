668dfd839d391dd5716e09af813b050835ebc195
waqasurrehman <Waqas ur Rehman@DESKTOP-UFGC6IS>
2018-01-10 23:45:31 +0500

    first

---
+++ online-shoopingbackend/src/main/java/net/waqas/online_shoopingbackend/App.java
@@ -0,0 +1,13 @@
+package net.waqas.online_shoopingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
